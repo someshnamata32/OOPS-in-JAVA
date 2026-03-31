0d659dc9aa95f92595a73b320d3f4f4c2f167274
Somesh Namta <somesh.namata32@gmail.com>
2026-04-01 01:53:43 +0530

    Create Question3.java

---
+++ String/Question3/Question3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class pr3 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String str = sc.nextLine();
+
+        String words[] = str.split(" ");
+
+        for(int i=words.length-1;i>=0;i--){
+            System.out.print(words[i] + " ");
+        }
+    }
+}
