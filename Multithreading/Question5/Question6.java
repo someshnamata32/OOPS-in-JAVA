f3fd4896d486bebbc0460ffcc5772a98e11e2714
Somesh Namta <somesh.namata32@gmail.com>
2026-04-05 20:34:24 +0530

    Refactor multithreading example for square and factorial
    Refactored multithreading example to calculate square and factorial. Removed unused thread and improved output formatting.

--- Multithreading/Question5/Question6.java
+++ Multithreading/Question5/Question6.java
@@ -1,55 +1,42 @@
-class myThread extends Thread {
-    public void run() {
-        System.out.println("Child Thread (Numbers)");
-        for(int i = 1; i <= 10; i++) {
-            System.out.println("Number " + i);
-        }
-    }
-}
 import java.util.*;
 
 class myThread1 extends Thread {
     int num;
+    int square;
 
     myThread1(int num) {
         this.num = num;
     }
 
     public void run() {
-        System.out.println("Square Thread running");
-        System.out.println("Square = " + (num * num));
+        square = num * num;
+        System.out.println("Square = " + square);
+    }
+
+    public int getSquare() {
+        return square;
     }
 }
 
-
-public class pr5{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-		System.out.println("Enter number ");
-		int num = sc.nextInt();
-
-        myThread1 t1 = new myThread1(num);
-         t1.start();
-
-			int fact = 1;
-			for(int i = 1; i <= num; i++) {
-				fact *= i;
-			}
-        System.out.println("Factorial Thread running");
-        System.out.println("Factorial = " + fact);
-		}
-    }
-
 public class pr5 {
     public static void main(String[] args) throws Exception {
-        myThread t = new myThread();
-        t.start();
+        Scanner sc = new Scanner(System.in);
 
-        t.join(); 
+        System.out.print("Enter number: ");
+        int num = sc.nextInt();
 
-        System.out.println("Main Thread (Characters)");
-        for(char c = 'A'; c <= 'J'; c++) {
-            System.out.println("Char " + c);
+        myThread1 t1 = new myThread1(num);
+        t1.start();
+
+        t1.join(); 
+
+        int square = t1.getSquare();
+
+        int fact = 1;
+        for(int i = 1; i <= square; i++) {
+            fact *= i;
         }
+
+        System.out.println("Factorial = " + fact);
     }
 }
